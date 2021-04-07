7227f29edd840046095aa16e00e8d0c1c08c1af1
rafalq <kruszewskirafael@gmail.com>
2021-04-07 01:00:32 +0100

    Like and Unlike system for Reply

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\LikeController;
 use App\Http\Controllers\ReplyController;
 
 Route::apiResource('/question', QuestionController::class);
 Route::apiResource('/category', CategoryController::class);
 Route::apiResource('/question/{question}/reply', ReplyController::class);
+
+Route::post('like/{reply}', [LikeController::class, 'like']);
+Route::delete('like/{reply}', [LikeController::class, 'unlike']);
